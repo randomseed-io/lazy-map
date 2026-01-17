029a67127c32c84719f5859707c096506d1d0f39
Paweł Wilk <siefca@gnu.org>
2026-01-17 16:28:08 +0100

    Codox configuration EDN moved to dev. resources

--- aliases/codox/io/randomseed/lazy_map/codox/main.clj
+++ aliases/codox/io/randomseed/lazy_map/codox/main.clj
@@ -1,39 +1,47 @@
 (ns io.randomseed.lazy-map.codox.main
+
   (:require [codox.main                      :as       c]
             [clojure.java.io                 :as      io]
             [clojure.edn                     :as     edn]
             [clojure.tools.deps              :as    deps]
             [clojure.string                  :as     str]))
 
+
+(defn- read-edn-config
+  [f]
+  (when-let [url (io/resource (or (not-empty f) "codox.edn"))]
+    (with-open [r (java.io.PushbackReader. (io/reader url))]
+      (edn/read r))))
+
 (defn- read-deps
   []
   (:project-edn (deps/find-edn-maps)))
 
-(defn- get-options [deps & more]
-  (let [key     (or (first more) :codox)
-        version (or (second more) "0.0.0")
-        codox   (key deps)
+(defn- get-options [deps config version]
+  (let [version (or version "0.0.0")
+        codox   config
         userdir (System/getProperty "user.dir")]
     (merge {:source-paths (:paths deps ["src"])
             :root-path    (:root-path deps userdir)
             :output-path  (str (io/file (:target deps userdir) "docs"))}
            codox
            {:name        (str/capitalize (:name codox (:name deps)))
-            :license     (:license     codox (:license deps))
-            :package     (:package     codox (:license deps))
+            :license     (:license     codox (:license     deps))
+            :package     (:package     codox (:license     deps))
             :description (:description codox (:description deps))
-            :version     (or version (:version codox (:version deps)))})))
+            :version     (or version (:version deps) (:version codox))})))
 
 (defn codox
   "Generate API documentation from source code."
-  [key version]
+  [version]
   (let [version (when version (str version))
         deps    (read-deps)
-        options (get-options deps key version)]
+        config  (read-edn-config "codox.edn")
+        options (get-options deps config version)]
     (codox.main/generate-docs options)
     (shutdown-agents)
     (println "Generated HTML docs in" (:output-path options))))
 
 (defn -main
-  [& args]
-  (codox :codox (first args)))
+  [version]
+  (codox version))
